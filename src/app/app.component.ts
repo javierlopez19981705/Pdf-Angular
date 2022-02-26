6525315961fa8648fdc1f1bad537a077733ef975
Javier Lopez <javierlopez19981705@gmail.com>
2022-02-25 17:14:23 -0800

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import html2canvas from 'html2canvas';
+import { jsPDF } from "jspdf";
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,33 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'testpdf';
+
+  constructor() {
+  }
+
+
+   downloadPDF() {
+    // Extraemos el
+    const DATA:any = document.getElementById('openpay-reference-pdf');
+    const doc = new jsPDF('p', 'pt', 'letter');
+    const options = {
+      background: 'white',
+      scale: 3
+    };
+    html2canvas(DATA, options).then((canvas) => {
+
+      const img = canvas.toDataURL('image/PNG');
+
+      // Add image Canvas to PDF
+      const bufferX = 15;
+      const bufferY = 15;
+      const imgProps = (doc as any).getImageProperties(img);
+      const pdfWidth = doc.internal.pageSize.getWidth() - 2 * bufferX;
+      const pdfHeight = (imgProps.height * pdfWidth) / imgProps.width;
+      doc.addImage(img, 'PNG', bufferX, bufferY, pdfWidth, pdfHeight, undefined, 'FAST');
+      return doc;
+    }).then((docResult) => {
+      docResult.save(`${new Date().toISOString()}_tutorial.pdf`);
+    });
+  }
 }
